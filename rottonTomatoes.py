d60fa25de3f26e7e57cf05ad463f50920d79e09a
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2023-09-01 22:23:50 -0400

    simplify rottonTomatoes.py

--- rottonTomatoes.py
+++ rottonTomatoes.py
@@ -11,17 +11,14 @@ def rotten_data():
 
     rotten_soup = BeautifulSoup(results_rotten.text, "html.parser")
 
-    rotten_rank = []
     rotten_name = []
     rotten_score = []
 
     for tag in rotten_soup.find_all('h2'):
         name = tag.find('a')
-        rank = tag.find('span', class_='subtle start-year')
         score = tag.find('span', class_='tMeterScore')
-        if name is not None and rank is not None:
+        if name is not None and score is not None:
             rotten_name.append(name.get_text())
-            rotten_rank.append(rank.get_text())
             rotten_score.append(score.get_text())
 
     return rotten_name
